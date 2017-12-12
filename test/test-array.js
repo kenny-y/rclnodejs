d19a3af203484aa729a13e56a01d0bfe600a3614
Kenny Yuan <kaining.yuan@intel.com>
2017-12-12 15:27:24 +0800

    Adding Array.copy() test cases for TypedArray and also Int64/UInt64 array

--- test/test-array.js
+++ test/test-array.js
@@ -17,6 +17,11 @@
 const assert = require('assert');
 const childProcess = require('child_process');
 const rclnodejs = require('../index.js');
+const translator = require('../lib/message_translator.js');
+
+function isTypedArray(v) {
+  return ArrayBuffer.isView(v) && !(v instanceof DataView)
+}
 
 describe('rclnodejs message communication', function() {
   this.timeout(60 * 1000);
@@ -51,5 +56,138 @@ describe('rclnodejs message communication', function() {
       }
     });
     rclnodejs.spin(node);
+  });
+
+
+  /* eslint-disable camelcase */
+  /* eslint-disable key-spacing */
+  /* eslint-disable comma-spacing */
+
+  const layout = {
+    dim: [
+      {label: 'height',  size: 10, stride: 600},
+      {label: 'width',   size: 20, stride: 60},
+    ],
+    data_offset: 0,
+  };
+
+  [
+    {
+      pkg: 'std_msgs', type: 'Int8MultiArray',
+      values: [
+        {layout: layout, data: [-10, 1, 2, 3, 8, 6, 0, -25] }, // Provide data via Array
+        {layout: layout, data: Int8Array.from([-10, 1, 2, 3, 8, 6, 0, -25]) }, // Provide data via TypedArray
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'Int16MultiArray',
+      values: [
+        {layout: layout, data: [-10, 1, 2, 3, 8, 6, 0, -25] }, // Provide data via Array
+        {layout: layout, data: Int16Array.from([-10, 1, 2, 3, 8, 6, 0, -25]) }, // Provide data via TypedArray
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'Int32MultiArray',
+      values: [
+        {layout: layout, data: [-10, 1, 2, 3, 8, 6, 0, -25] }, // Provide data via Array
+        {layout: layout, data: Int32Array.from([-10, 1, 2, 3, 8, 6, 0, -25]) }, // Provide data via TypedArray
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'Int64MultiArray',
+      values: [
+        {layout: layout, data: [-111, 1, 2, 3, 8, 6, 0, -25, Number.MAX_SAFE_INTEGER] }, // Provide data via Array
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'UInt8MultiArray',
+      values: [
+        {layout: layout, data: [0, 1, 2, 3, 8, 6, 0, 255] }, // Provide data via Array
+        {layout: layout, data: Uint8Array.from([0, 1, 2, 3, 8, 6, 0, 255]) }, // Provide data via TypedArray
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'UInt16MultiArray',
+      values: [
+        {layout: layout, data: [0, 1, 2, 3, 8, 6, 0, 255] }, // Provide data via Array
+        {layout: layout, data: Uint16Array.from([0, 1, 2, 3, 8, 6, 0, 255]) }, // Provide data via TypedArray
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'UInt32MultiArray',
+      values: [
+        {layout: layout, data: [0, 1, 2, 3, 8, 6, 0, 255] }, // Provide data via Array
+        {layout: layout, data: Uint32Array.from([0, 1, 2, 3, 8, 6, 0, 255]) }, // Provide data via TypedArray
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'UInt64MultiArray',
+      values: [
+        {layout: layout, data: [0, 1, 2, 3, 8, 6, 0, 255, Number.MAX_SAFE_INTEGER] }, // Provide data via Array
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'Float32MultiArray',
+      values: [
+        {layout: layout, data: [-10, 1, 2, 3, 8, 6, 0, -25] }, // Provide data via Array
+        {layout: layout, data: Float32Array.from([-10, 1, 2, 3, 8, 6, 0, -25]) }, // Provide data via TypedArray
+      ]
+    },
+    {
+      pkg: 'std_msgs', type: 'Float64MultiArray',
+      values: [
+        {layout: layout, data: [-10, 1, 2, 3, 8, 6, 0, -25] }, // Provide data via Array
+        {layout: layout, data: Float64Array.from([-10, 1, 2, 3, 8, 6, 0, -25]) }, // Provide data via TypedArray
+      ]
+    },
+  /* eslint-enable camelcase */
+  /* eslint-enable key-spacing */
+  /* eslint-enable comma-spacing */
+  ].forEach((testData) => {
+    const topic = testData.topic || 'topic' + testData.type;
+    testData.values.forEach((v, i) => {
+      it('Test ' + testData.type + '.copy()' +  ', case ' + i, function() {
+        const MessageType = rclnodejs.require(testData.pkg + '/msg/' + testData.type);
+        const msg1 = translator.toROSMessage(MessageType, v);
+        const msg2 = new MessageType();
+        msg2.copy(msg1);
+
+        function checkMessage(msg) {
+          assert(typeof msg.layout === 'object');
+
+          assert(Array.isArray(msg.layout.dim.data));
+          // First element
+          assert(typeof msg.layout.dim.data[0] === 'object');
+          assert(msg.layout.dim.data[0].label === v.layout.dim[0].label);
+          assert(msg.layout.dim.data[0].size === v.layout.dim[0].size);
+          assert(msg.layout.dim.data[0].stride === v.layout.dim[0].stride);
+          // Second element
+          assert(typeof msg.layout.dim.data[1] === 'object');
+          assert(msg.layout.dim.data[1].label === v.layout.dim[1].label);
+          assert(msg.layout.dim.data[1].size === v.layout.dim[1].size);
+          assert(msg.layout.dim.data[1].stride === v.layout.dim[1].stride);
+
+          assert(msg.layout.data_offset === v.layout.data_offset);
+
+          assert(msg.data);
+          if (isTypedArray(v.data)) {
+            assert.deepStrictEqual(msg.data, v.data);
+          } else {
+            assert.deepEqual(msg.data, v.data);
+          }
+        }
+
+        checkMessage(msg1);
+        checkMessage(msg2);
+
+        const o = v.data[0];
+        assert(msg2.data[0] == o);
+        const r = Math.round(Math.random() * 100);
+        msg1.data[0] = r;
+        assert(msg1.data[0] == r);
+        console.log(r, msg1.data[0], msg2.data[0], o);
+        assert(msg2.data[0] == o);
+      });
+    });
   });
 });
